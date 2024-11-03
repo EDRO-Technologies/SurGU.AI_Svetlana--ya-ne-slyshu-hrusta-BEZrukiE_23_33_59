b8e05b7dc65be9c9dc57dc382b0c4e2f76e7806f
holy-jesus <hj@holy-coder.ru>
2024-11-03 06:15:46 +0500

    Переназвал класс с `TakVamVidno` в `TVV`; Начал его реализовывать

--- TakVamVidno/takvamvidno.py
+++ TakVamVidno/takvamvidno.py
@@ -1,6 +1,19 @@
-class TakVamVidno:
-    def __init__(self):
-        pass
+from .extractors.images.base import OCR
+from .extractors.voice.base import STT
+from .extractors import FileType, get_file_type
 
-    def process(data: bytes):
-        pass
+
+class TVV:
+    def __init__(
+        self, image_extractor: OCR | None = None, audio_extractor: STT | None = None
+    ):
+        self._image_extractor = image_extractor
+        self._audio_extractor = audio_extractor
+
+    def process(self, text: str = None, file: list[bytes] = None):
+        if text is None and file is None:
+            raise ValueError("text и file не могут быть одновременно 'None'")
+        file_type = get_file_type(file)
+        if file_type != FileType.Image:
+            # Пока не поддерживается
+            return None
